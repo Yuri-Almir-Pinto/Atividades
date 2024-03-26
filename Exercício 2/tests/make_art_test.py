fb6ac9ef6609c096d254445340fcb03947cf8ab5
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-03-25 22:31:04 -0300

    Removido atividade repetida, passado a usar o env do python normal.

--- Exercício 2/tests/make_art_test.py
+++ Exercício 2/tests/make_art_test.py
@@ -1,7 +1,7 @@
-import sys
 import cv2
-import numpy as np
-from testes_4 import make_art
+from projects import make_art
+from contextlib import redirect_stdout
+from io import StringIO
 
 def get_resized_image_shape(image):
     height, width = image.shape
@@ -10,21 +10,38 @@ def get_resized_image_shape(image):
 
     return resized_img
 
-def test_print_out_ascii():
-    make_art.print_out_ascii([
-        [0,1,2,3,0,1,2,3],
-        [3,2,1,0,3,2,1,0]
-    ])
+def test_print_out_ascii_with_image() -> None:
+    compare_ascii: str = ""
+    with open('./artifacts/ascii.txt', 'r') as file:
+        for line in file:
+            compare_ascii += line
 
-def test_img_to_ascii():
-    image = cv2.imread("./tests/jerma.png", 0)
+    error_image = cv2.imread("./artifacts/error_image.png", 0)
+
+    output = make_art.img_to_ascii(error_image)
+
+    f = StringIO()
+    with redirect_stdout(f):
+        make_art.print_out_ascii(output)
+
+    assert f.getvalue() == compare_ascii
+    
+
+def test_print_out_ascii():
+    f = StringIO()
+    with redirect_stdout(f):
+        make_art.print_out_ascii([
+            [0,1,2,3,4,5,0,1,2,3,4,5],
+            [5,4,3,2,1,0,5,4,3,2,1,0]
+        ])
+
+    assert f.getvalue() == "#-*.+o#-*.+o\no+.*-#o+.*-#\n"
+
+def test_img_format():
+    image = cv2.imread("./artifacts/jerma.png", 0)
 
     new_img = get_resized_image_shape(image)
 
     output = make_art.img_to_ascii(image)
 
     assert output.shape == new_img.shape
-
-print("batata")
-
-test_print_out_ascii()
--- Exercício 2/tests/random_password_gen_test.py
+++ Exercício 2/tests/random_password_gen_test.py
@@ -5,7 +5,7 @@ def test_generate_pass():
     for i in range(10000):
         gen_password = password.generate_pass(10)
         if gen_password in password_check.keys():
-            assert False, "Houve senha repetida."
+            assert False, "Teve senha repetida."
         else:
             password_check[gen_password] = True
         
